2ddfef1675ed7011a6ca2b249e7492bf5d170755
Thraix <thraix@hotmail.com>
2019-05-13 15:53:28 +0200

    Improve inventory UI

--- src/InventoryScene.h
+++ src/InventoryScene.h
@@ -11,24 +11,42 @@ class InventoryScene : public Greet::Layer
 {
   private:
     const Inventory& inventory;
+    Greet::Renderable2D renderable;
+    Greet::Renderable2D selected;
 
   public:
     InventoryScene(const Inventory& inventory)
-      : Layer{new Greet::BatchRenderer(), Greet::ShaderFactory::DefaultShader(), Greet::Mat3::Orthographic(0,Greet::Window::GetWidth(), 0, Greet::Window::GetHeight())}, inventory{inventory}
+      : Layer{new Greet::BatchRenderer(), Greet::ShaderFactory::DefaultShader(), Greet::Mat3::Orthographic(0,Greet::Window::GetWidth(), 0, Greet::Window::GetHeight())}, inventory{inventory}, renderable{{10.0f,0},{64,64},0xffffffff, {Greet::TextureManager::Get2D("material")}}, selected{{10.0f,0},{64,64},0xffffffff, {Greet::TextureManager::Get2D("selected_material")}} 
     {
 
     }
 
     void Render() const override
     {
+      Greet::Renderable2D renderableCpy = renderable;
+      Greet::Renderable2D selectedCpy = selected;
       Greet::BatchRenderer* renderer = (Greet::BatchRenderer*)m_renderer; 
       size_t size = inventory.GetInventorySize();
+      float height = (size * 70.0f) - 6.0f;
       for(int i = 0;i<size;i++)
       {
         const Voxel& voxel = inventory.GetMaterial(i);
         float amount = inventory.GetMaterialAmount(i);
-        std::string astrix = inventory.GetCurrentPos() == i ? "* " : "";
-        renderer->SubmitString(astrix + voxel.name + ": " + std::to_string((int)amount), {0.0f, 20.0f + 30.0f * i}, Greet::FontManager::Get("roboto", 20), 0xffffffff);
+
+        std::string amountStr = std::to_string((int)amount);
+        float yPos = (Greet::Window::GetHeight() - height) * 0.5f + 70.0f * i;
+        renderableCpy.m_color = Greet::ColorUtils::Vec4ToColorHex(voxel.color);
+        renderableCpy.m_position.y = yPos;
+        renderer->Submit(renderableCpy);
+
+        Greet::Font* font = Greet::FontManager::Get("roboto", 20);
+        if(inventory.GetCurrentPos() == i)
+        {
+
+          selectedCpy.m_position = renderableCpy.m_position;
+          renderer->Submit(selectedCpy);
+        }
+        renderer->SubmitString(amountStr, {10.0f + 32 - font->GetWidthOfText(amountStr)* 0.5f, yPos + 60.0f}, font, 0xffffffff);
       }
     }
 };
